025f554260bb1a324f18c2a002db1118146cddff
v-9469 <146191952+v-9469@users.noreply.github.com>
2024-08-08 12:10:46 +0530

    Added function to calculate gpa and total marks cleaned up code

--- extract/extract_table.py
+++ extract/extract_table.py
@@ -1,6 +1,12 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 
+credit_4=["BMATS101","BPHYS102"]
+credit_3=["BPOPS103","BESCK104B","BETCK105H"]
+credit_2=[]
+credit_1=["BENGK106","BKSKK107","BIDTK158"]
+
+
 
 def extractor(saved_html):
     with open(saved_html, "r", encoding="utf-8") as file:
@@ -16,4 +22,43 @@ def extractor(saved_html):
         table_data.append(row_data)
 
     df_marks = pd.DataFrame(table_data[1:9], columns=table_data[0])
+
+
+    sub_status=list(df_marks["Result"])
+    grade=[]
+    if "F" not in sub_status:
+        for i in list(df_marks["Total"]) :
+            if int(i) >=90:
+                grade.append(10)
+            elif int(i)>=80:
+                grade.append(9)
+            elif int(i)>=70:
+                grade.append(8)
+            elif int(i)>=60:
+                grade.append(7)
+            elif int(i)>=50:
+                grade.append(6)
+            elif int(i)>=40:
+                grade.append(5)
+
+        #print(grade)
+
+        df_marks.insert(6,"Grade Points",grade)
+
+        credit_obtained=[]
+        for i,j in zip(list(df_marks["Grade Points"]),list(df_marks["Subject Code"])):
+            if j in credit_4:
+                credit_obtained.append(4*i)
+            elif j in credit_3:
+                credit_obtained.append(3*i)
+            elif j in credit_2:
+                credit_obtained.append(2*i)
+            if j in credit_1:
+                credit_obtained.append(1*i)
+
+        df_marks.insert(7,"Credits Obtained",credit_obtained)
+
     return df_marks
+
+
+
