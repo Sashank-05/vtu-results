373239eee8dc6d969ca3cb1f53471c736c72ab8c
v-9469 <146191952+v-9469@users.noreply.github.com>
2024-08-11 00:09:43 +0530

    Fixed database creation and inserting records

--- helper/dbhandler.py
+++ helper/dbhandler.py
@@ -1,87 +1,46 @@
-import sqlite3
+import sqlite3 
 
-
-class DBHandler:
-    def __init__(self, db_path='database.db', student_db_path='students.db'):
-        self.db_path = db_path
-        self.student_db_path = student_db_path
-        self.initialize_database()
-        self.initialize_student_database()
+class db_handler:
+    def __init__(self, db_path='database.db'):
+        self.db_path=db_path
 
     def connect(self, db_path):
         return sqlite3.connect(db_path)
+        
+    def create_table_cloumns(self,sub_columns:list): #table name also should be a parameter which should be taken from user
+        sql_create_table="""CREATE TABLE BI23CD_SEM_1(
+                    USN VARCHAR(12),
+                    NAME VARCHAR(25))
+            """
+        connection=self.connect(self.db_path)
+        cur=connection.cursor()
+        cur.execute(sql_create_table)
+        connection.commit()
 
-    def initialize_database(self):
-        create_branch_table_query = """
-        CREATE TABLE IF NOT EXISTS branches (
-            branch_id TEXT PRIMARY KEY,
-            branch_name TEXT NOT NULL
-        );
-        """
-        create_marks_table_query = """
-        CREATE TABLE IF NOT EXISTS marks (
-            usn TEXT,
-            branch_id TEXT,
-            semester INTEGER,
-            subject TEXT,
-            marks INTEGER,
-            FOREIGN KEY(branch_id) REFERENCES branches(branch_id)
-        );
-        """
-        conn = self.connect(self.db_path)
-        cursor = conn.cursor()
-        cursor.execute(create_branch_table_query)
-        cursor.execute(create_marks_table_query)
-        conn.commit()
-        conn.close()
+        for sub in sub_columns:
+            sql = f"ALTER TABLE BI23CD_SEM_1 ADD {sub} VARCHAR(100);"
+            cur.execute(sql)
+            connection.commit()
 
-    def initialize_student_database(self):
-        create_student_table_query = """
-        CREATE TABLE IF NOT EXISTS students (
-            usn TEXT PRIMARY KEY,
-            name TEXT NOT NULL,
-            branch_id TEXT,
-            FOREIGN KEY(branch_id) REFERENCES branches(branch_id)
-        );
-        """
-        conn = self.connect(self.student_db_path)
-        cursor = conn.cursor()
-        cursor.execute(create_student_table_query)
-        conn.commit()
-        conn.close()
+        connection.close()
 
-    def get_student_marks(self, usn):
-        query = """
-        SELECT * FROM marks
-        WHERE usn = ?
-        """
-        conn = self.connect(self.student_db_path)
-        cursor = conn.cursor()
-        cursor.execute(query, (usn,))
-        result = cursor.fetchall()
-        conn.close()
-        return result
+    def push_data_into_table(self, inte: list, ext: list,  other: list):
+        sql = """INSERT INTO BI23CD_SEM_1 VALUES
+            (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )"""
+        values = (other[0], other[1], inte[0], ext[0], inte[1], ext[1], inte[2], ext[2], 
+              inte[3], ext[3], inte[4], ext[4], inte[5], ext[5], inte[6], ext[6],
+              inte[7], ext[7],  other[2], other[3], other[4])
+        connection=self.connect(self.db_path)
+        cur=connection.cursor()
+        cur.execute(sql, values)
+        connection.commit()
 
-    def get_branch_list(self, branch_id):
-        query = """
-        SELECT * FROM branches
-        WHERE branch_id = ?
-        """
-        conn = self.connect(self.db_path)
-        cursor = conn.cursor()
-        cursor.execute(query, (branch_id,))
-        result = cursor.fetchall()
-        conn.close()
-        return result
 
-    def get_semester_marks(self, branch_id, sem):
-        query = """
-        SELECT * FROM marks
-        WHERE branch_id = ? AND semester = ?
-        """
-        conn = self.connect(self.db_path)
-        cursor = conn.cursor()
-        cursor.execute(query, (branch_id, sem))
-        result = cursor.fetchall()
-        conn.close()
-        return result
+        
+        
+
+
+
+    
+
+
