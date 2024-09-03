fa5ec16a8bd0aa712c8c113ebd7624df5e78781b
Sashank-05 <sashankvanka@outlook.com>
2024-09-03 18:31:35 +0530

    websockets for live scraping

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+import logging
+import threading
+
 from flask import Flask, render_template, jsonify, redirect, url_for, request
+from flask_socketio import SocketIO
 
 from helper import dbhandler, test, threadedgetdata
 
-app = Flask(__name__)
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
+app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
+socketio = SocketIO(app)
 db = dbhandler.DBHandler()
 
 db.connect()
@@ -16,39 +24,14 @@ def home():
     return render_template("home.html")
 
 
-"""@app.route(apiv1 + 'student/<string:usn>', methods=['GET'])
-def get_student(usn):
-    
-    apiv1/student/1BI23CD001
-
-    Get's the student's marks card from 1st to 8th semester
-
-    
-    try:
-        student_marks = dict()
-        cols = []
-        for i in range(1, 8 + 1):
-            try:
-                student_marks[i] = list(db.get_student_marks("BI23CD", i, usn))
-                cols.append(db.get_columns(f"BI23CD_SEM_{i}"))
-                print(student_marks, cols)
-            except Exception as e:
-                if "no such table" in str(e):
-                    pass
-                else:
-                    return jsonify({'error': str(e)}), 500
-        return jsonify(student_marks, cols), 200
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500"""
-
 @app.route(apiv1 + "export/<string:type>")
 def export(type):
     if type == "excel":
         # return a file to download
-        
+        raise NotImplementedError
     else:
         return jsonify({"error": "Type not Found"})
+
 
 @app.route(apiv1 + 'student/<string:usn>', methods=['GET'])
 def get_student(usn):
@@ -66,20 +49,11 @@ def get_student(usn):
 
 @app.route(apiv1 + '<string:id>/<string:sem>', methods=['GET'])
 def get_sem_marks(id, sem):
-    """
-
-    apiv1/1BI23CD/1
-
-    returns the semester marks of all students belonging to 1BI23CD
-
-    """
     try:
         sem_marks = db.get_semester_marks(id, sem)
         columns = db.get_columns(f"BI23CD_SEM_{sem}")
-
         return jsonify(sem_marks, columns)
     except Exception as e:
-        # if error is table not found then redirect to scrape page
         if "no such table" in str(e):
             return redirect(url_for('scrape', usn_prefix=id))
         else:
@@ -94,25 +68,47 @@ def scrape():
 @app.route(apiv1 + 'scrape', methods=['POST'])
 def scrape_student():
     data = request.json
-    usn_prefix = data['usn_prefix']
+    usn_prefix = data.get('usn_prefix')
     custom_url = data.get('custom_url')
     sem = data.get('sem')
     end_usn = data.get('end_usn')
     num_threads = data.get('num_threads', 4)
 
-    if (not custom_url or not usn_prefix or not custom_url or not sem or not sem.isdigit() or not end_usn or
-            not end_usn.isdigit()):
+    if not usn_prefix or not custom_url or not sem or not sem.isdigit() or not end_usn or not end_usn.isdigit():
         return jsonify({'success': False, 'error': 'Please provide valid data'}), 400
 
     try:
-        handler = threadedgetdata.ThreadManager(custom_url, usn_prefix, f"{usn_prefix[1::]}_SEM_{sem}", end_usn, num_threads=num_threads)
-        handler.run_threads()
+        handler = threadedgetdata.ThreadManager(custom_url, usn_prefix, f"{usn_prefix[1:]}_SEM_{sem}", end_usn,
+                                                num_threads=num_threads, socketio=socketio)
 
-        
+        # after defining threads tell the server a message and start the threads without using socketio
+        threading.Thread(target=handler.run_threads).start()
+
         return jsonify({'success': True}), 200
+
+
+    except ValueError as ve:
+        app.logger.error(f"ValueError: {ve}")
+        return jsonify({'success': False, 'error': str(ve)}), 400
     except Exception as e:
-        return jsonify({'success': False, 'error': str(e)}), 500
+        app.logger.error(f"Exception: {e}")
+        return jsonify({'success': False, 'error': 'An unexpected error occurred'}), 500
+
+
+@app.route('/live')
+def live_scrape():
+    return render_template("live.html")
+
+
+@socketio.on('connect')
+def handle_connect():
+    app.logger.info('Client connected')
+
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    app.logger.info('Client disconnected')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True, port=8000)
--- helper/threadedgetdata.py
+++ helper/threadedgetdata.py
@@ -5,22 +5,25 @@ import threading
 import cv2 as cv
 import pytesseract
 import selenium.common.exceptions
+from flask_socketio import emit
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from helper import extract_table, df_to_csv, processing, dbhandler
+
 from captcha import Captcha
+from helper import extract_table, df_to_csv, processing, dbhandler
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
 
 class FillForm:
-    def __init__(self, base_url, usn_prefix, start_range, end_range, db_table):
+    def __init__(self, base_url, usn_prefix, start_range, end_range, db_table, socketio):
         self.base_url = base_url
         self.usn_prefix = usn_prefix
         self.start_range = start_range
         self.end_range = end_range
         self.db_table = db_table
         self.fail_count = 0
+        self.socketio = socketio
 
         print(f"Starting thread for USN range: {self.start_range} to {self.end_range} (-_-\")")
 
@@ -81,6 +84,7 @@ class FillForm:
             if "Student Name" in self.driver.page_source:
                 with open(f"pages/{usn}.html", "w", encoding="utf8") as file:
                     file.write(self.driver.page_source)
+                    self.socketio.emit('update', {'usn': usn}, namespace='/')
             else:
                 print(f"Couldn't Save page for USN: {usn}")
                 self.fail_count += 1
@@ -108,18 +112,17 @@ class FillForm:
             self.fill_form(usn)
 
         print(f"Total failed attempts: {self.fail_count}")
-    
-    
 
 
 class ThreadManager:
-    def __init__(self, base_url, usn_prefix, db_table, end_usn=None, ranges=None, num_threads=4):
+    def __init__(self, base_url, usn_prefix, db_table, end_usn=None, ranges=None, num_threads=4, socketio=None):
         self.base_url = base_url
         self.usn_prefix = usn_prefix
         self.ranges = ranges
         self.db_table = db_table
         self.num_threads = num_threads
         self.end_usn = int(end_usn)
+        self.socketio = socketio
 
         if end_usn is None and ranges is None:
             raise ValueError("Either end_usn or ranges must be provided")
@@ -131,7 +134,8 @@ class ThreadManager:
         threads = []
         for r in self.ranges:
             start_range, end_range = r
-            fill_form_instance = FillForm(self.base_url, self.usn_prefix, start_range, end_range, self.db_table)
+            fill_form_instance = FillForm(self.base_url, self.usn_prefix, start_range, end_range, self.db_table,
+                                          self.socketio)
             thread = threading.Thread(target=fill_form_instance.run)
             threads.append(thread)
 
@@ -140,15 +144,16 @@ class ThreadManager:
 
         for thread in threads:
             thread.join()
-        
-        self.save_to_db()
-        
 
+        self.save_to_db()
+
+        emit('update', {'usn': 'done'}, namespace='/')
 
     def div_usns(self):
         # divide into ranges
         total_usns = int(self.end_usn)
         num_threads = int(self.num_threads)
+
         base_range_size = int(total_usns // num_threads)
         remainder = int(total_usns % num_threads)
 
@@ -160,29 +165,31 @@ class ThreadManager:
             current_end = current_start + range_size - 1
             ranges.append((current_start, current_end))
             current_start = current_end + 1
+
         return ranges
-    
-    def save_to_db(self):
-        files = os.listdir("pages")
-        print(files)
-        x, y = extract_table.extractor(f"pages/{files[0]}")
-        df, _ = extract_table.cal(x, y)
-        columns = processing.get_subject_code(df)
 
-        table = dbhandler.DBHandler()
-        try:
-            table.create_table_columns(self.db_table, columns)
-        except:
-            print("Table already created")
-            
-        for file in files:
-            x, y = extract_table.extractor(f"pages/{file}")
-            df_new, other = extract_table.cal(x, y)
-            df_to_csv.convert(df_new, other)
-            inte, ext, lis = processing.df_to_sql(df_new, other)
-            table.push_data_into_table(self.db_table, inte, ext, lis)
 
-        table.close()
+def save_to_db(self):
+    files = os.listdir("pages")
+    print(files)
+    x, y = extract_table.extractor(f"pages/{files[0]}")
+    df, _ = extract_table.cal(x, y)
+    columns = processing.get_subject_code(df)
+
+    table = dbhandler.DBHandler()
+    try:
+        table.create_table_columns(self.db_table, columns)
+    except:
+        print("Table already created")
+
+    for file in files:
+        x, y = extract_table.extractor(f"pages/{file}")
+        df_new, other = extract_table.cal(x, y)
+        df_to_csv.convert(df_new, other)
+        inte, ext, lis = processing.df_to_sql(df_new, other)
+        table.push_data_into_table(self.db_table, inte, ext, lis)
+
+    table.close()
 
 
 if __name__ == "__main__":
